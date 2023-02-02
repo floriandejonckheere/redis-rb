58a8f795673229ddaa8c1712f6a3601e5cdd9cc9
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-02-02 21:26:04 +0200

    Parse command-line arguments

--- lib/redis/cli.rb
+++ lib/redis/cli.rb
@@ -5,6 +5,32 @@ require "English"
 
 module Redis
   class CLI
+    attr_reader :options
+
+    def initialize(args)
+      # Default options
+      @options = {
+        host: "127.0.0.1",
+        port: 6379,
+        verbose: false,
+      }
+
+      # Parse command-line arguments
+      parser.parse!(args, into: @options)
+    end
+
     def start; end
+
+    private
+
+    def parser
+      @parser ||= OptionParser.new("#{File.basename($PROGRAM_NAME)} [options]") do |o|
+        o.on("-v", "--verbose", "Turn on verbose logging")
+        o.on("-h", "--host HOST", "Host to bind to")
+        o.on("-p", "--port PORT", Integer, "Port to bind to")
+
+        o.on("--help", "Display this message") { abort(parser.to_s) }
+      end
+    end
   end
 end
