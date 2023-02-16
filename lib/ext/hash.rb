3f48ed3140914a61a6335fe546d217d3546ac0d9
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-02-16 16:37:47 +0100

    Replace custom map type with Hash

---
+++ lib/ext/hash.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+# typed: true
+
+class Hash
+  extend T::Sig
+
+  sig { returns(String) }
+  def to_resp3
+    "%#{count * 2}\r\n#{flat_map { |k, v| [k.to_resp3, v.to_resp3] }.join}"
+  end
+
+  sig { params(_type: String, socket: Redis::Socket, block: T.proc.returns(Redis::Type)).returns(T.attached_class) }
+  def self.from_resp3(_type, socket, &block)
+    # Read number of elements in hash
+    count = socket.gets.chomp.to_i
+
+    # Read elements
+    elements = Array.new(count, &block)
+
+    # Convert to pair-wise array, and then to hash
+    elements
+      .each_slice(2)
+      .to_h
+  end
+end
