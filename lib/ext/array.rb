dc8f5851456aaa49c85344df5eaa6f95542f1e9b
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-02-16 16:37:26 +0100

    Replace custom array type with Array

---
+++ lib/ext/array.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+# typed: true
+
+class Array
+  extend T::Sig
+
+  sig { returns(String) }
+  def to_resp3
+    "*#{count}\r\n#{map(&:to_resp3).join}"
+  end
+
+  sig { params(_type: String, socket: Redis::Socket, block: T.proc.returns(Redis::Type)).returns(T.attached_class) }
+  def self.from_resp3(_type, socket, &block)
+    # Read number of elements in array
+    count = socket.gets.chomp.to_i
+
+    # Read elements
+    new(count, &block)
+  end
+end
---
+++ spec/ext/array_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+# typed: true
+
+RSpec.describe Array do
+  subject(:type) { described_class.new(value) }
+
+  let(:value) { ["hello", "world"] }
+
+  let(:pipes) { IO.pipe }
+
+  let(:rsocket) { Redis::Socket.new(pipes.first) }
+  let(:wsocket) { pipes.last }
+
+  let(:parser) { Redis::Types::Parser.new(rsocket) }
+
+  describe "#to_resp3" do
+    it "serializes the type" do
+      expect(type.to_resp3).to eq "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"
+    end
+  end
+
+  describe ".from_resp3" do
+    it "deserializes the type" do
+      wsocket.write("2\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
+
+      type = described_class.from_resp3("*", rsocket, &parser.method(:read))
+
+      expect(type).to eq ["hello", "world"]
+    end
+  end
+end
