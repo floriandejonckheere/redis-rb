3a8918dc953900756861abe9661d3c36bf909c3a
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-02-20 21:42:56 +0100

    Add support for inf, -inf and nan in doubles

--- lib/ext/float.rb
+++ lib/ext/float.rb
@@ -6,13 +6,25 @@ class Float
 
   sig { returns(String) }
   def to_resp3
-    ",#{self}\r\n"
+    ",#{'-' if negative?}#{infinite? || nan? ? abs.to_s.downcase[0..2] : self}\r\n"
   end
 
   sig { params(_type: String, socket: Redis::Socket).returns(T.attached_class) }
   def self.from_resp3(_type, socket)
-    Float socket
+    # Read value
+    value = socket
       .gets
       .chomp
+
+    case value.downcase
+    when "inf"
+      Float::INFINITY
+    when "-inf"
+      -Float::INFINITY
+    when "nan"
+      Float::NAN
+    else
+      Float value
+    end
   end
 end
--- spec/ext/float_spec.rb
+++ spec/ext/float_spec.rb
@@ -14,6 +14,30 @@ RSpec.describe Float do
     it "serializes the type" do
       expect(type.to_resp3).to eq ",1.23\r\n"
     end
+
+    context "when the value is not a number" do
+      let(:value) { Float::NAN }
+
+      it "serializes the type" do
+        expect(value.to_resp3).to eq ",nan\r\n"
+      end
+    end
+  end
+
+  context "when the value is infinity" do
+    let(:value) { Float::INFINITY }
+
+    it "serializes the type" do
+      expect(type.to_resp3).to eq ",inf\r\n"
+    end
+  end
+
+  context "when the value is negative infinity" do
+    let(:value) { -Float::INFINITY }
+
+    it "serializes the type" do
+      expect(value.to_resp3).to eq ",-inf\r\n"
+    end
   end
 
   describe ".from_resp3" do
@@ -23,6 +47,36 @@ RSpec.describe Float do
       type = described_class.from_resp3(",", rsocket)
 
       expect(type).to eq 1.23
+    end
+
+    context "when the value is not a number" do
+      it "deserializes the type" do
+        wsocket.write("nan\r\n")
+
+        type = described_class.from_resp3(",", rsocket)
+
+        expect(type).to be_nan
+      end
+    end
+
+    context "when the value is infinity" do
+      it "deserializes the type" do
+        wsocket.write("inf\r\n")
+
+        type = described_class.from_resp3(",", rsocket)
+
+        expect(type).to be_infinite
+      end
+    end
+
+    context "when the value is negative infinity" do
+      it "deserializes the type" do
+        wsocket.write("-inf\r\n")
+
+        type = described_class.from_resp3(",", rsocket)
+
+        expect(type).to be_infinite
+      end
     end
   end
 end
