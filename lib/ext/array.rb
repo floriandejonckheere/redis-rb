520631e73d30bccaffc496cea9b0371d0a839c5a
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-03-05 09:24:42 +0200

    Add Array#to_pretty_s

--- lib/ext/array.rb
+++ lib/ext/array.rb
@@ -18,6 +18,18 @@ class Array
     "*#{count}\r\n#{map { |e| T.cast(e, Rediss::Type).to_resp3 }.join}"
   end
 
+  sig { override.params(indent: Integer).returns(String) }
+  def to_pretty_s(indent: 0)
+    max_length = count.to_s.length
+
+    # First, indent each element. Then, right justify the index of each element by the maximum length of the index.
+    # Then, pretty print the value recursively and replace the newlines with spaces (normal indent plus right justify
+    # of array indices: maximum length of the index plus the parenthesis plus a space).
+    each_with_index
+      .map { |e, i| "#{INDENT * indent}#{i.to_s.rjust(max_length)}) #{e.to_pretty_s.gsub(/\n/, "\n#{INDENT * indent}#{' ' * (max_length + 2)}")}" }
+      .join("\n")
+  end
+
   sig { override.params(type: String, socket: Rediss::Socket, block: T.proc.returns(Rediss::Type)).returns(T.attached_class) }
   def self.from_resp3(type, socket, &block)
     # Read number of elements in array
--- spec/ext/array_spec.rb
+++ spec/ext/array_spec.rb
@@ -26,6 +26,62 @@ RSpec.describe Array do
     end
   end
 
+  describe "#to_pretty_s" do
+    it "pretty prints the type" do
+      expect(type.to_pretty_s).to eq "0) hello\n1) world"
+    end
+
+    context "when the indent is non-zero" do
+      let(:value) { ["hello", "world"] }
+
+      it "indents the values" do
+        expect(type.to_pretty_s(indent: 2)).to eq "    0) hello\n    1) world"
+      end
+    end
+
+    context "when the array contains a lot of elements" do
+      let(:value) { ["hello"] * 11 }
+
+      it "right justifies the values" do
+        expect(type.to_pretty_s).to eq " 0) hello\n 1) hello\n 2) hello\n 3) hello\n 4) hello\n 5) hello\n 6) hello\n 7) hello\n 8) hello\n 9) hello\n10) hello"
+      end
+
+      context "when the indent is non-zero" do
+        it "right justifies the values" do
+          expect(type.to_pretty_s(indent: 2)).to eq "     0) hello\n     1) hello\n     2) hello\n     3) hello\n     4) hello\n     5) hello\n     6) hello\n     7) hello\n     8) hello\n     9) hello\n    10) hello"
+        end
+      end
+    end
+
+    context "when the array contains nested arrays" do
+      let(:value) { ["hello", ["world"]] }
+
+      it "pretty prints the type" do
+        expect(type.to_pretty_s).to eq "0) hello\n1) 0) world"
+      end
+
+      context "when the indent is non-zero" do
+        it "indents the values" do
+          expect(type.to_pretty_s(indent: 2)).to eq "    0) hello\n    1) 0) world"
+        end
+      end
+
+      context "when the nested array contains a lots of elements" do
+        let(:value) { ["hello", ["world"] * 11] }
+
+        it "pretty prints the type" do
+          expect(type.to_pretty_s).to eq "0) hello\n1)  0) world\n    1) world\n    2) world\n    3) world\n    4) world\n    5) world\n    6) world\n    7) world\n    8) world\n    9) world\n   10) world"
+        end
+
+        context "when the indent is non-zero" do
+          it "indents the values" do
+            expect(type.to_pretty_s(indent: 2)).to eq "    0) hello\n    1)  0) world\n        1) world\n        2) world\n        3) world\n        4) world\n        5) world\n        6) world\n        7) world\n        8) world\n        9) world\n       10) world"
+          end
+        end
+      end
+    end
+  end
+
   describe ".from_resp3" do
     it "deserializes the type" do
       wsocket.write("2\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
