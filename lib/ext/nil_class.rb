906b47e79e7c1672cc5ef1eaa4c869fa46c04510
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-03-05 10:05:26 +0200

    Add NilClass#to_pretty_s

--- lib/ext/nil_class.rb
+++ lib/ext/nil_class.rb
@@ -13,6 +13,11 @@ class NilClass
     "_\r\n"
   end
 
+  sig { override.params(indent: Integer).returns(String) }
+  def to_pretty_s(indent: 0)
+    "(nil)"
+  end
+
   # Returns untyped because Sorbet is giving a cryptic error when trying to use `T.attached_class`
   sig { override.params(type: String, socket: Rediss::Socket, block: T.proc.returns(Rediss::Type)).returns(T.untyped) }
   def self.from_resp3(type, socket, &block)
